e12e1e24486bf0bb7777960dec532b12dca8f85a
Alice Cash <alice@alice.cash>
2021-05-02 01:32:14 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2018 Matthew Cash
+# Copyright 2018 Alice Cash
 #
 # Redistribution and use in source and binary forms, with or without modification,
 # are permitted provided that the following conditions are met:
